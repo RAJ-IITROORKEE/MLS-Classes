b070c3a100ea6a3941a1236ced7027ccd26efee7
RAJ-IITROORKEE <raj_r@mt.iitr.ac.in>
2026-06-07 16:32:33 +0530

    fix tiptap table imports

--- app/components/admin/rich-text-editor.tsx
+++ app/components/admin/rich-text-editor.tsx
@@ -8,10 +8,7 @@ import TextAlign from '@tiptap/extension-text-align';
 import { TextStyle } from '@tiptap/extension-text-style';
 import Color from '@tiptap/extension-color';
 import Highlight from '@tiptap/extension-highlight';
-import Table from '@tiptap/extension-table';
-import TableRow from '@tiptap/extension-table-row';
-import TableHeader from '@tiptap/extension-table-header';
-import TableCell from '@tiptap/extension-table-cell';
+import { Table, TableCell, TableHeader, TableRow } from '@tiptap/extension-table';
 import {
   Bold,
   Italic,
